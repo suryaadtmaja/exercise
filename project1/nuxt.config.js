c00f86a060f919d5a3cc2652e1683b7b60e373f0
Surya Adtmaja <suryaadtmaja13@gmail.com>
2018-03-21 22:45:00 +0700

    first commit

---
+++ project1/nuxt.config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'project1',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'}
+    ],
+    script: [
+      { src: 'https://code.jquery.com/jquery-3.2.1.slim.min.js' },
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js'},
+      { src: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'}
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  modules: [
+      '@nuxtjs/axios',
+  ],
+
+  axios: {
+      // proxyHeaders: false
+  },
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
---
+++ project1/pages/posts/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <h1>Blog</h1>
+    <div class="row">
+        <div v-for="(post, index) in posts" :key="index" class="col-md-3">
+          <div class="card">
+            <img class="card-img-top" :src="post.thumbnailUrl" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">{{post.title}}</h5>
+              <p class="card-text">{{post.title}}</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    asyncData({ req, params }) {
+        return axios.get('https://jsonplaceholder.typicode.com/photos')
+            .then((res) => {
+                return { posts: res.data.slice(0, 5) }
+            })
+    },
+    head: {
+        title: 'List posts'
+    }
+}
+</script>
