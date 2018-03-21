6430c071653df0b6c0bb492fd5e771319ed687e2
Surya Adtmaja <suryaadtmaja13@gmail.com>
2018-03-22 00:22:26 +0700

    adding link to detail images in post

---
+++ project1/pages/posts/_id.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container mt-5 mb-5">
+    <div class="row">
+    <div class="col">
+      <img :src="post.url">
+    </div>
+    <div class="col">
+      <h1 style="font-size: 1.5rem">{{post.title}}</h1>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    asyncData({ req, params }) {
+        return axios.get('https://jsonplaceholder.typicode.com/photos/' + params.id)
+            .then((res) => {
+                return { post: res.data }
+            })
+    },
+    head() {
+    return {
+        title: 'List posts'
+    }
+    },
+}
+</script>
